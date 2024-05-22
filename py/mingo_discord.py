df64f6185464a4d0ce967d7bfcec94c1165abb35
Schön, Philipp <philipp.schoen@student.fhws.de>
2024-05-22 09:16:33 +0200

    refactor

--- py/mingo_discord.py
+++ py/mingo_discord.py
@@ -2,65 +2,72 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('c_serial_port/gyro_verify-2.csv', delimiter=';')
-gyroX = df.iloc[0:200, 0]
+df = pd.read_csv('c_serial_port/data_flugzeug.csv', delimiter=';')
+messurement_data_gyro_axis_x = df.iloc[:, 0]
 
 
 # Anzahl der Messwerte
-gyroXarrSize = len(gyroX)
+messurement_sample_size = len(messurement_data_gyro_axis_x)
 
 # Median der Messwerte
-gyroXmedian = np.median(gyroX)
+median_of_messurement_data = np.median(messurement_data_gyro_axis_x)
 
 # Temperaturbias (Drift) berechnen
-gyroXbias = gyroX - gyroXmedian
+gyroXbias = messurement_data_gyro_axis_x - median_of_messurement_data
 
-# Varianz der Messergebnisse (f체r ein anschauliches Beispiel verwenden wir 0.1)
+# Varianz der Messergebnisse (für ein anschauliches Beispiel verwenden wir 0.1)
 gyroXbiasVarianz = 0.1
+
 
 # Anwendung der Filteriterationen
 
 # Varianz der Messergebnisse wird durch R dargestellt
-R = gyroXbiasVarianz
+varianz_der_messung = gyroXbiasVarianz
 
 # Einheitsmatrix, hier numerisch, deshalb = 1
 I = 1
 
 # Initiale Vorbelegung
-filterVals = np.zeros(gyroXarrSize)
-kalmanVals = np.zeros(gyroXarrSize)
-xkSub1 = 0
-xk = xkSub1  # Vorheriger Messwert unbekannt, deshalb 0
+filter_collected_values_array = np.zeros(messurement_sample_size)
+kalman_collected_values_array = np.zeros(messurement_sample_size)
+estimate_previous_iteration = 0
+# Vorheriger Messwert unbekannt, deshalb 0
+estimate_current_iteration = estimate_previous_iteration
 
 # Fehlerkovarianz am Anfang 1 und nicht 0, da normalerweise immer ein Messrauschen vorhanden ist
-PkSub1 = 1
-Pk = PkSub1
+error_covariance_previous_iteration = 1
+error_covariance_current_iteration = error_covariance_previous_iteration
 
-# Iteration 1: Erste Sch채tzung
-zk = gyroXbias[0]
-Kk = Pk / (Pk + R)
-kalmanVals[0] = Kk
-xk = xk + Kk * (zk - xk)
-filterVals[0] = xk
-Pk = (I - Kk) * Pk
+# Iteration 1: Erste Schätzung
+winkelgeschwindigkeit = gyroXbias[0]
+kalman_gain_current_iteration = error_covariance_current_iteration / \
+    (error_covariance_current_iteration + varianz_der_messung)
+kalman_collected_values_array[0] = kalman_gain_current_iteration
+estimate_current_iteration = estimate_current_iteration + \
+    kalman_gain_current_iteration * \
+    (winkelgeschwindigkeit - estimate_current_iteration)
+filter_collected_values_array[0] = estimate_current_iteration
+error_covariance_current_iteration = (
+    I - kalman_gain_current_iteration) * error_covariance_current_iteration
 
 # Iteration 2 bis gyroXarrSize
-for j in range(1, gyroXarrSize):
+for iteration in range(1, messurement_sample_size):
+
     # Vorhersage
-    zk = gyroXbias[j]  # Aktuelle Messung
-    xk = filterVals[j - 1]  # xk = xk-1
+    winkelgeschwindigkeit = gyroXbias[iteration]  # Aktuelle Messung
+    # xk = xk-1
+    estimate_current_iteration = filter_collected_values_array[iteration - 1]
 
     # Korrektur
-    Kk = Pk / (Pk + R)
-    kalmanVals[j] = Kk
-    xk = xk + Kk * (zk - xk)
-    filterVals[j] = xk
-    Pk = (I - Kk) * Pk
-
-# fig, (ax1, ax2) = plt.subplots(1, 2)
-# fig.suptitle('Horizontally stacked subplots')
-# ax1.plot(x, y)
-# ax2.plot(x, -y)
+    kalman_gain_current_iteration = error_covariance_current_iteration / \
+        (error_covariance_current_iteration + varianz_der_messung)
+    kalman_collected_values_array[iteration] = kalman_gain_current_iteration
+    estimate_current_iteration = estimate_current_iteration + \
+        kalman_gain_current_iteration * \
+        (winkelgeschwindigkeit - estimate_current_iteration)
+    filter_collected_values_array[iteration] = estimate_current_iteration
+    error_covariance_current_iteration = (
+        I - kalman_gain_current_iteration) * error_covariance_current_iteration
 
 
 # Graph plotten
