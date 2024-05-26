f24a6f3eb8d16154c68a2aacb6cffe38499f8f8c
Schön, Philipp <philipp.schoen@student.fhws.de>
2024-05-26 15:50:50 +0200

    variables

--- py/mingo_discord.py
+++ py/mingo_discord.py
@@ -3,34 +3,34 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # [Abstract:]
-# 
+#
 # Der nachfolgende Code zeigt eine vereinfachte Anwendung des Kalman Filters.
-# Beim Kalman Filter handelt es sich um einen Zustandsschätzer, welcher mittels eines iterativen 
+# Beim Kalman Filter handelt es sich um einen Zustandsschätzer, welcher mittels eines iterativen
 # Ansatzes dazu beitragen kann, sich dem reellen Zustand eines Systems anzunähren. Dabei wird bei
-# jeder Iteration zunächst eine Vorhersage getroffen, mit Hilfe der gemessenen Daten eine Korrektur 
-# vorgenommen, und anschließend besagte Korrektur im nächsten Schritt für die nächste Vorhersage 
+# jeder Iteration zunächst eine Vorhersage getroffen, mit Hilfe der gemessenen Daten eine Korrektur
+# vorgenommen, und anschließend besagte Korrektur im nächsten Schritt für die nächste Vorhersage
 # verwendet.
-# 
+#
 # Der Nachfolgende Ablauf im hier Vorhandenen Code ist wie folgt:
 #   1. Die Messdaten werden eingelesen, und die ersten benötigten Werte berechnet
-#   2. Für den Initialen Setup wird die erste Iteration mit besagten Werten teils Hardgecoded 
+#   2. Für den Initialen Setup wird die erste Iteration mit besagten Werten teils Hardgecoded
 #      gesetzt, sodass die weiteren Iterationen ohne weiteres Zutun erfolgen können.
 #   3. Beginn der Loop über die Messdaten:
 #       3.1 Vorhersage:
 #           - Die Fehlerkovarianz wird vorrausberechnet (hier vereinfacht, daher gleich dem Wert der
 #             vorherigen Iteration)
-#           - Der aktuelle Schätzwert wird auf den in der vorherigen Iteration ermittelten, 
-#             bereits korrigierten Wert gesetzt 
+#           - Der aktuelle Schätzwert wird auf den in der vorherigen Iteration ermittelten,
+#             bereits korrigierten Wert gesetzt
 #       3.2 Korrektur:
 #           - Der Kalman Gain wird neu berechnet
 #           - Die Schätzung wird mit der gemessenen Winkelgeschwindigkeit aktualisieren
 #           - Die Fehlerkovarianz wird aktualisieren
 #       3.3 Die Loop endet und es beginnt die nächste Iteration
-# 
+#
 #   4. Erstelen eines Plotts
-# 
+#
 #  > Bedeutung der Mathematischen Zeichen:
-# 
+#
 #  * --------------------------------------------------------------------------------------------- *
 #  | Zeichen | Bedeutung                                                                           |
 #  | ------- | ----------------------------------------------------------------------------------- |
@@ -52,46 +52,53 @@ import matplotlib.pyplot as plt
 # Auslagerung der Funktionen / Mathematik
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-
 
-def get_next_estimate(x_kprev,A =1, B = 0,uprev = 0):
-    x_k = A * x_k-1 + B * uprev
-    return 1
 
-def get_next_error_estimation( P_kprev, A = 1,AT = 1,Q = 0):
-    P_k = A * P_k-1 * AT + Q 
+def get_next_estimate(x_kprev, A=1, B=0, uprev=0):
+    x_k = A * x_kprev + B * uprev
+    return x_k
+
+
+def get_next_error_estimation(P_kprev, A=1, AT=1, Q=0):
+    P_k = A * P_kprev * AT + Q
     return P_k
+
 
 def receive_new_messurement(iteration):
     return messurement_bias[iteration]
 
+
 def update_kalman_gain(P_k, R, HT, H):
     # [3.] Den Kalman Gain berechnen
-    K_k = P_k * HT * ( 1 / (H * P_k * H^T + R) )
+    K_k = P_k * HT * (1 / (H * P_k * H ^ T + R))
     return K_k
 
-def update_estimate(x_k, K_k, z_k, H = 1):
+
+def update_estimate(x_k, K_k, z_k, H=1):
     # [4.] Die Schätzung mit der gemessenen Winkelgeschwindigkeit aktualisieren
     x_k = x_k + K_k(z_k - H * x_k)
     filter_collected_values_array[iteration] = x_k
     return x_k
 
-def update_error_estimate(P_k, I, K_k, H = 1):
+
+def update_error_estimate(P_k, I, K_k, H=1):
     # [5.] Die Fehlerkovarianz aktualisieren
-    P_k = ( I - K_k * H) * P_k
+    P_k = (I - K_k * H) * P_k
     return P_k
 
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-
 # Setup
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-
 
+
 df = pd.read_csv('c_serial_port/data_flugzeug.csv', delimiter=';')
-messurement_data_gyro_axis_x = df.iloc[:, 0]
+messurement_data_gyro_axis_x = df.iloc[:, 1]
 # Anzahl der Messwerte
 messurement_sample_size = len(messurement_data_gyro_axis_x)
 # Median der Messwerte
 median_of_messurement_data = np.median(messurement_data_gyro_axis_x)
 # Temperaturbias (Drift) berechnen
 messurement_bias = messurement_data_gyro_axis_x - median_of_messurement_data
-# Varianz der Messergebnisse (für ein anschauliches Beispiel verwenden wir 0.1)
+# Varianz der Messergebnisse (für ein anschauliches Beispiel verwenden wir 0.58)
 messurement_bias_varianz = 0.58
 # Varianz der Messergebnisse wird durch R dargestellt
 varianz_der_messung = messurement_bias_varianz
@@ -122,30 +129,34 @@ error_covariance_current_iteration = error_covariance_previous_iteration
 # Messung vornehmen:
 messurement_current = messurement_bias[0]
 # [3.] Den Kalman Gain berechnen
-kalman_gain_current_iteration = error_covariance_current_iteration / (error_covariance_current_iteration + varianz_der_messung)
+kalman_gain_current_iteration = error_covariance_current_iteration / \
+    (error_covariance_current_iteration + varianz_der_messung)
 kalman_collected_values_array[0] = kalman_gain_current_iteration
 
 # [4.] Die Schätzung mit der gemessenen Winkelgeschwindigkeit aktualisieren
-estimate_current_iteration = estimate_current_iteration + kalman_gain_current_iteration * (messurement_current - estimate_current_iteration)
+estimate_current_iteration = estimate_current_iteration + \
+    kalman_gain_current_iteration * \
+    (messurement_current - estimate_current_iteration)
 filter_collected_values_array[0] = estimate_current_iteration
 
 # [5.] Die Fehlerkovarianz aktualisieren
-error_covariance_previous_iteration = (I - kalman_gain_current_iteration) * error_covariance_current_iteration
+error_covariance_previous_iteration = (
+    I - kalman_gain_current_iteration) * error_covariance_previous_iteration
 
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-
 # -- Iteration 2 bis messurement_sample_size
 
 for iteration in range(1, messurement_sample_size):
 
-    # -- Vorhersage - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-    
+    # -- Vorhersage - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
     # [1.] Den nächsten Zustand darstellen
     # x_k = x_k-1                                                      [x_k = A * x_k-1 + B * u_k-1]
     estimate_current_iteration = filter_collected_values_array[iteration - 1]
 
     # [2.] Die Fehlerkovarianz vorrausberechnen
     # P_k = P_k-1                                                       [P_k = A * P_k-1 * A^T + Q ]
-    error_covariance_current_iteration =  error_covariance_previous_iteration
+    error_covariance_current_iteration = error_covariance_previous_iteration
 
     # -- Korrektur - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -153,17 +164,21 @@ for iteration in range(1, messurement_sample_size):
 
     # [3.] Den Kalman Gain berechnen
     # K_k = P_k * (P_k + R)^-1                            [K_k = P_k * H^T * (H * P_k * H^T + R)^-1]
-    kalman_gain_current_iteration = error_covariance_current_iteration / (error_covariance_current_iteration + varianz_der_messung)
+    kalman_gain_current_iteration = error_covariance_current_iteration / \
+        (error_covariance_current_iteration + varianz_der_messung)
     kalman_collected_values_array[iteration] = kalman_gain_current_iteration
 
     # [4.] Die Schätzung mit der gemessenen Winkelgeschwindigkeit aktualisieren
     # x_k + K_k * (z_k - x_k)                                       [x_k = x_k + K_k(z_k - H * x_k)]
-    estimate_current_iteration = estimate_current_iteration + kalman_gain_current_iteration * (messurement_current - estimate_current_iteration)
+    estimate_current_iteration = estimate_current_iteration + \
+        kalman_gain_current_iteration * \
+        (messurement_current - estimate_current_iteration)
     filter_collected_values_array[iteration] = estimate_current_iteration
 
     # [5.] Die Fehlerkovarianz aktualisieren
     # P_k = (I - K_k) * P_k                                            [ P_k = ( I - K_k * H) * P_k]
-    error_covariance_previous_iteration = (I - kalman_gain_current_iteration) * error_covariance_current_iteration
+    error_covariance_previous_iteration = (
+        I - kalman_gain_current_iteration) * error_covariance_current_iteration
 
     # -- Next Iteration  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -175,9 +190,11 @@ for iteration in range(1, messurement_sample_size):
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-
 
 fig, axs = plt.subplots(2)
-axs[1].bar(range(messurement_sample_size), kalman_collected_values_array, label='Kk: Kalman Gain', linestyle='-', color=[0, 0.75, 1], edgecolor='b')
+axs[1].bar(range(messurement_sample_size), kalman_collected_values_array,
+           label='Kk: Kalman Gain', linestyle='-', color=[0, 0.75, 1], edgecolor='b')
 axs[0].plot(messurement_bias, '-', label='zk: Messwerte', color='red')
-axs[0].plot(filter_collected_values_array, color='green', linewidth=2, label='xk: gefilterte Werte')
+axs[0].plot(filter_collected_values_array, color='green',
+            linewidth=2, label='xk: gefilterte Werte')
 axs[0].axhline(0, color='blue', label='reales Signal')
 
 # Legende anzeigen
