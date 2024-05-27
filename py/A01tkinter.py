e92fe99446d2f6b84e68a307c8218c6566247e04
Constantin Thein <constantin.thein@gmail.com>
2024-05-27 18:27:32 +0200

    prettified

--- py/A01tkinter.py
+++ py/A01tkinter.py
@@ -2,18 +2,27 @@ import tkinter as tk
 import random
 import numpy as np
 
-# measurement interval in ms
+# -- KALMAN PREFERENCES
 measurement_interval = 20
-
-delta_t = 0 #1 #measurement_interval / 1000
-
 sigma = 15
 draw_phase = 0
 real_point = None
-states = []
+
+# -- GUI CONFIGURATION
 canvas_width = 800
-canvas_height = 600
+canvas_height = int(0.75 * canvas_width)
 route_padding = 40
+
+dot_radius = 2
+# // TODO: FInd good Math equation for constant step size 
+step_size = 120 
+
+tail = 20
+
+# -- SETUP
+dot_x = route_padding
+dot_y = route_padding
+
 # State Transition
 A = np.array([
     [1, 0, 0.2, 0],
@@ -59,7 +68,6 @@ I = np.eye(4)
 
 c = np.zeros((4, 1))
 
-
 def calculate_kalman(noisy_x, noisy_y, prev_noisy_x, prev_noisy_y):
     global x, x_prev, P, P_prev 
     x[0, 0] = prev_noisy_x
@@ -96,26 +104,11 @@ root.title("Kalman Example in 2D - Visualisation")
 canvas = tk.Canvas(root, width=canvas_width, height=canvas_height, bg='#1F1F31')
 canvas.pack()
 
-
-# Draw the initial dot (circle)
-dot_radius = 2
-noise_dot_radius = 2
-initial_x = 40
-initial_y = 40
-tail = 20
-noise_factor = 5 #0.5
-step_size = 8
-# Initial position
-dot_x = initial_x
-dot_y = initial_y
-
-
-
 def oval_create(x, y):
     return canvas.create_oval(x - dot_radius, y - dot_radius, x + dot_radius, y + dot_radius, fill='blue', outline='')
 
 def noise_oval_create(x, y):
-    return canvas.create_oval(x - noise_dot_radius, y - noise_dot_radius, x + noise_dot_radius, y + noise_dot_radius, fill='lightgreen', outline='')
+    return canvas.create_oval(x - dot_radius, y - dot_radius, x + dot_radius, y + dot_radius, fill='lightgreen', outline='')
 
 def noise_line_create(x, y, prev_x, prev_y):
     return canvas.create_line(x, y, prev_x, prev_y, fill='lightgreen', width='2')
@@ -147,13 +140,13 @@ canvas.tag_lower(rectangle)
 def check_position():
     global dot_x, dot_y, route_padding
     if (dot_x < (canvas_width - route_padding) and dot_y == route_padding):
-        dot_x = dot_x + step_size
+        dot_x = dot_x + (canvas_width - 2 * route_padding) / step_size
     elif (dot_x == (canvas_width - route_padding) and dot_y < (canvas_height - route_padding)):
-        dot_y = dot_y + step_size
+        dot_y = dot_y + (canvas_height - 2 * route_padding) / step_size
     elif (dot_x > route_padding and dot_y == (canvas_height - route_padding)):
-        dot_x = dot_x - step_size
+        dot_x = dot_x - (canvas_width - 2 * route_padding) / step_size
     else:
-        dot_y = dot_y - step_size
+        dot_y = dot_y - (canvas_height - 2 * route_padding) / step_size
 
 def add_noise():
     global dot_x, dot_y
@@ -169,12 +162,11 @@ def update_canvas(new_x, new_y):
     noise_x, noise_y = add_noise()
     prev_noise_temp = noise_dots[len(noise_dots)-1]
     prev_noise_dot = canvas.coords(prev_noise_temp)
-    prev_noise_x = prev_noise_dot[0] + noise_dot_radius
-    prev_noise_y = prev_noise_dot[1] + noise_dot_radius
+    prev_noise_x = prev_noise_dot[0] + dot_radius
+    prev_noise_y = prev_noise_dot[1] + dot_radius
     print(f"-- previous Noise point was: {prev_noise_x}, {prev_noise_y}\n")
 
-    kalman_x, kalman_y = calculate_kalman(
-        noise_x, noise_y, prev_noise_x, prev_noise_y)
+    kalman_x, kalman_y = calculate_kalman(noise_x, noise_y, prev_noise_x, prev_noise_y)
 
     print(f"Kalman received: {noise_x}, {noise_y}, {prev_noise_x}, {prev_noise_y}\n")
     print(f"kalman x:\t{kalman_x},\nkalman y:\t{kalman_y}\n")
@@ -188,7 +180,7 @@ def update_canvas(new_x, new_y):
     prev_line   = canvas.coords( noise_lines[len(noise_lines )-1])
     prev_kalman = canvas.coords(kalman_lines[len(kalman_lines)-1])
 
-    canvas.coords(noise_temp, noise_x - noise_dot_radius, noise_y - noise_dot_radius, noise_x + noise_dot_radius, noise_y + noise_dot_radius)
+    canvas.coords(noise_temp, noise_x - dot_radius, noise_y - dot_radius, noise_x + dot_radius, noise_y + dot_radius)
     canvas.coords(oval_temp, dot_x - dot_radius, dot_y - dot_radius, dot_x + dot_radius, dot_y + dot_radius)
     canvas.coords(line_temp, prev_line[2], prev_line[3], noise_x, noise_y)
     canvas.coords(kalman_temp, prev_kalman[2], prev_kalman[3], kalman_x, kalman_y)
@@ -202,11 +194,5 @@ def update_canvas(new_x, new_y):
     check_position()
     root.after(measurement_interval, update_canvas, dot_x, dot_y)
 
-
-
-# Example of updating the dot's position after 2 seconds
-# Move dot to (300, 300) after 2 seconds
 root.after(measurement_interval, update_canvas, dot_x, dot_y)
-
-# Run the Tkinter event loop
 root.mainloop()
