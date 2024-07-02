f6db2946dd70f459e182a8ec6208adc0cf3cec40
Schön, Philipp <philipp.schoen@student.fhws.de>
2024-07-02 20:42:05 +0200

    tunnel

--- py/deprecated/KalmanExplained.py
+++ py/deprecated/KalmanExplained.py
@@ -91,13 +91,17 @@ def update_error_estimate(P_k, I, K_k, H=1):
 
 
 df = pd.read_csv('c_serial_port/data_flugzeug.csv', delimiter=';')
-messurement_data_gyro_axis_x = df.iloc[:, 1]
+messurement_data_gyro_axis_x = [22.22, 22.22, 22.22, 22.22, 22.22, 22.22,
+                                22.22, 22.22, 22.22, 22.22, 22.22, 22.22, 22.22, 22.22, 22.22]+np.random.randn(15)
+# messurement_data_gyro_axis_x = [
+#     500.0, 495.1, 490.4, 455.9, 421.5, 377.4, 323.4, 259.7, 186.1, 102.7, 9.5]
 # Anzahl der Messwerte
+
 messurement_sample_size = len(messurement_data_gyro_axis_x)
 # Median der Messwerte
 median_of_messurement_data = np.median(messurement_data_gyro_axis_x)
 # Temperaturbias (Drift) berechnen
-messurement_bias = messurement_data_gyro_axis_x - median_of_messurement_data
+messurement_bias = messurement_data_gyro_axis_x  # - median_of_messurement_data
 # Varianz der Messergebnisse (für ein anschauliches Beispiel verwenden wir 0.58)
 messurement_bias_varianz = 0.58
 # Varianz der Messergebnisse wird durch R dargestellt
@@ -128,10 +132,14 @@ error_covariance_current_iteration = error_covariance_previous_iteration
 
 # Messung vornehmen:
 messurement_current = messurement_bias[0]
+
+print(messurement_current)
 # [3.] Den Kalman Gain berechnen
 kalman_gain_current_iteration = error_covariance_current_iteration / \
     (error_covariance_current_iteration + varianz_der_messung)
 kalman_collected_values_array[0] = kalman_gain_current_iteration
+
+print(kalman_gain_current_iteration)
 
 # [4.] Die Schätzung mit der gemessenen Winkelgeschwindigkeit aktualisieren
 estimate_current_iteration = estimate_current_iteration + \
@@ -139,12 +147,29 @@ estimate_current_iteration = estimate_current_iteration + \
     (messurement_current - estimate_current_iteration)
 filter_collected_values_array[0] = estimate_current_iteration
 
+print(estimate_current_iteration)
+
 # [5.] Die Fehlerkovarianz aktualisieren
 error_covariance_previous_iteration = (
     I - kalman_gain_current_iteration) * error_covariance_previous_iteration
 
+print(error_covariance_previous_iteration)
+
 # -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-
 # -- Iteration 2 bis messurement_sample_size
+
+k = []
+z_k = []
+K_k = []
+P_k = []
+dach_x_k = []
+
+k.append("1")
+z_k.append(f"{messurement_current}")
+K_k.append(f"{kalman_gain_current_iteration}")
+P_k.append(f"{error_covariance_current_iteration}")
+dach_x_k.append(f"{estimate_current_iteration}")
+
 
 for iteration in range(1, messurement_sample_size):
 
@@ -180,6 +205,14 @@ for iteration in range(1, messurement_sample_size):
     error_covariance_previous_iteration = (
         I - kalman_gain_current_iteration) * error_covariance_current_iteration
 
+    k.append(f"{iteration + 1}")
+    z_k.append(f"{messurement_current}")
+    K_k.append(f"{kalman_gain_current_iteration}")
+    P_k.append(f"{error_covariance_current_iteration}")
+    dach_x_k.append(f"{estimate_current_iteration}")
+
+    gesamt = [k, z_k, K_k, P_k, dach_x_k]
+
     # -- Next Iteration  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 
@@ -195,14 +228,14 @@ axs[1].bar(range(messurement_sample_size), kalman_collected_values_array,
 axs[0].plot(messurement_bias, '-', label='zk: Messwerte', color='red')
 axs[0].plot(filter_collected_values_array, color='green',
             linewidth=2, label='xk: gefilterte Werte')
-axs[0].axhline(0, color='blue', label='reales Signal')
+axs[0].axhline(22.22, color='blue', label='reales Signal')
 
 # Legende anzeigen
 axs[0].legend()
 
 # Achsenbeschriftung
-axs[0].set_xlabel('Zeit in ms')
-axs[0].set_ylabel('Winkelgeschwindigkeit in °/s')
+axs[0].set_xlabel('Zeit in s')
+axs[0].set_ylabel('Geschwindigkeit in m/s')
 
 
 # Bereich X-Achse anpassen
@@ -211,3 +244,6 @@ axs[1].set_xlim([0, messurement_sample_size])
 
 # Plot anzeigen
 plt.show()
+
+for array in gesamt:
+    print(' & '.join(map(str, array)))
